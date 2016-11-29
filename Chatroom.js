67236ea32ca4e033225da3a754b6c88e95dd622b
Gamze Dogan <gamze.dogan@student.reutlingen-university.de>
2016-11-29 14:22:10 +0100

    syntax fehler

--- Chatroom.js
+++ Chatroom.js
@@ -338,7 +338,7 @@ io.on('connection', function(socket) {
 		
 		request(url, function(error, response){
 			if(response.statusCode >= 200 && response.statusCode < 400){
-				console.log("response " + JSON.stringify(response);
+				console.log("response " + JSON.stringify(response));
 			} else {
 				console.log(error);
 			}
