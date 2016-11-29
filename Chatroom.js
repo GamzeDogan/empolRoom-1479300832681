41b119fabe57882cfd41b8fef7c2ed3c79ea9dab
Gamze Dogan <gamze.dogan@student.reutlingen-university.de>
2016-11-29 14:56:07 +0100

    latitude

--- Chatroom.js
+++ Chatroom.js
@@ -337,10 +337,10 @@ io.on('connection', function(socket) {
 		
 		request(urlStandort, function(error, response){
 			if(response.statusCode >= 200 && response.statusCode < 400){
-				// for(var i=0; i<response.locatin.length; i++){
+				// for(var i=0; i<response.location.length; i++){
 					// console.log(response.location[i]);
 				// }
-				console.log("response " + JSON.stringify(response.location[1]));
+				console.log("response " + JSON.stringify(response.location[latitude]));
 			} else {
 				console.log(error);
 			}
