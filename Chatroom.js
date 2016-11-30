efbd8246e3acfbb67052366e22935fe4683ef8e4
Gamze Dogan <gamze.dogan@student.reutlingen-university.de>
2016-11-30 19:59:41 +0100

    night

--- Chatroom.js
+++ Chatroom.js
@@ -345,7 +345,7 @@ io.on('connection', function(socket) {
 						if(response.statusCode >= 200 && response.statusCode < 400){
 							//filename = "./weathericons" + path.basename()
 							var content = JSON.parse(response.body);
-							console.log("body mit data: "+content.forecasts[0].num);
+							console.log("body mit data: "+content.forecasts[0].night.icon_code);
 						} else {
 							console.log("Error Message2: " + error);
 						}	
