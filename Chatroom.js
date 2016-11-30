f92c3cb0ac610424c2bf5debff22832ab7197910
Gamze Dogan <gamze.dogan@student.reutlingen-university.de>
2016-11-30 18:01:02 +0100

    lat und long

--- Chatroom.js
+++ Chatroom.js
@@ -334,7 +334,7 @@ io.on('connection', function(socket) {
 				console.log("long: "+longitude);
 			});
 	
-			request('https://'+weather.username+':'+weather.password+'@twcservice.mybluemix.net:443/api/weather/v1/geocode/45.42/75.69/forecast/daily/10day.json?units=m&language=en-US', function(error, response, body){
+			request('https://'+weather.username+':'+weather.password+'@twcservice.mybluemix.net:443/api/weather/v1/geocode/'+latitude+'/'+longitude+'/forecast/daily/10day.json?units=m&language=en-US', function(error, response, body){
 				if(response.statusCode >= 200 && response.statusCode < 400){
 					var content = JSON.parse(response.body);
 					console.log("body mit data: "+content.metadata.language);
